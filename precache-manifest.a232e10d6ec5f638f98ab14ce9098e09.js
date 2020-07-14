ea1b7cedb304a09cfe36fdbc30ce63a51781603e
Rishabh <rishabh.mehta98.rm@gmail.com>
2020-07-14 14:57:32 +0530

    Updates

---
+++ precache-manifest.a232e10d6ec5f638f98ab14ce9098e09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bead79970b1c8fcf218be002e46d0d69",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "6ba45bf13823ca6a2ba2",
+    "url": "/covid-tracker/static/css/main.ba577149.chunk.css"
+  },
+  {
+    "revision": "2dcc866d0a2cb7cc5a17",
+    "url": "/covid-tracker/static/js/2.175ef364.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-tracker/static/js/2.175ef364.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ba45bf13823ca6a2ba2",
+    "url": "/covid-tracker/static/js/main.bd3560c7.chunk.js"
+  },
+  {
+    "revision": "a2c09be945803fa375fb",
+    "url": "/covid-tracker/static/js/runtime-main.948b6ba1.js"
+  },
+  {
+    "revision": "781443a840212a97cb7ff0b067f3c9ca",
+    "url": "/covid-tracker/static/media/logo4.781443a8.png"
+  }
+]);
